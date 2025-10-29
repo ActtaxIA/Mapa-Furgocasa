2a11c377c28b8254f2070127db64a3c4ce603635
Tu Nombre <tu@email.com>
2025-10-29 23:09:18 +0100

    Anadir campo comunidad_autonoma al tipo Area

--- types/database.types.ts
+++ types/database.types.ts
@@ -22,6 +22,7 @@ export interface Database {
           ciudad: string | null
           provincia: string | null
           comunidad: string | null
+          comunidad_autonoma: string | null
           pais: string
           telefono: string | null
           email: string | null
@@ -58,6 +59,7 @@ export interface Database {
           ciudad?: string | null
           provincia?: string | null
           comunidad?: string | null
+          comunidad_autonoma?: string | null
           pais?: string
           telefono?: string | null
           email?: string | null
@@ -94,6 +96,7 @@ export interface Database {
           ciudad?: string | null
           provincia?: string | null
           comunidad?: string | null
+          comunidad_autonoma?: string | null
           pais?: string
           telefono?: string | null
           email?: string | null
